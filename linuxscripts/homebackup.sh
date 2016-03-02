eea89ef144c392ddff427339ff7268b624ffcc3c
bmccachren67 <mobbwar@gmail.com>
2016-03-02 12:57:10 -0500

    Added files via upload

---
+++ linuxscripts/homebackup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#This script is used to backup the user's home directory.
+
+#echo "Availiable home folders:"
+#ls /home
+#echo
+#echo "Which home folder would you like to backup?:"
+#read HOMEFOLDER
+#echo
+####################################################
+#Configuration Variables.
+####################################################
+HOMEFOLDER="ed"
+BACKUPDIR=/home
+TIMESTAMP=$(date | awk '{ print $1"_"$2"_"$3"_"$4"_"$5"_"$6 }')
+TARFILENAME="/var/tmp/$TIMESTAMP.$HOMEFOLDER.tar"
+GZIPFILENAME="$TARFILENAME.gz"
+REMOTESERVER="10.1.10.73"
+REMOTEUSER="administrator"
+REMOTEDIR="~"
+LOGFILE="/var/tmp/homebackuplog.txt"
+DELDATE=$(date -d "15 day ago" '+%a_%b_%d_%Y')
+DELTARFILE="$DELDATE.$HOMEFOLDER.tar"
+DELGZIPFILE="$DELTARFILE.gz"
+####################################################
+
+cd $BACKUPDIR
+
+if [ -f $TARFILENAME ]
+then
+rm $TARFILENAME
+fi
+
+tar -cf $TARFILENAME $HOMEFOLDER
+
+if [ -f $GZIPFILENAME ]
+then
+rm $GZIPFILENAME
+fi
+
+gzip $TARFILENAME
+
+sshpass -p "southhills#" scp $GZIPFILENAME $REMOTEUSER@$REMOTESERVER:$REMOTEDIR
+
+date >> $LOGFILE
+echo "Backup complete."
+
+cd /var/tmp
+rm  $DELDATE.$HOMEFOLDER.tar.gz
+
+echo "Backups 15 days ago have been removed."
